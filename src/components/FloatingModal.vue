c64ad4b323f6b0fe2cf22d361ef747bbdf662e6c
thatsillegal <zixuan.peng@gmail.com>
2024-10-29 02:11:04 +0800

    add popup window to show essays

---
+++ src/components/FloatingModal.vue
@@ -0,0 +1,70 @@
+<template>
+    <div v-if="isVisible" class="floating-modal" @click.self="closeModal">
+        <div class="modal-content">
+            <span class="close" @click="closeModal">
+                <bold>X</bold>
+            </span>
+            <!-- 显示传入的内容 -->
+            <iframe :src="content" frameborder='0' scrolling='no'></iframe>
+        </div>
+    </div>
+</template>
+
+<!-- width='100%' height='100vh' frameborder='0' scrolling='no' -->
+
+<script>
+export default {
+    props: {
+        content: {
+            type: String,
+            required: true
+        },
+        isVisible: {
+            type: Boolean,
+            required: true
+        }
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close'); // 触发关闭事件
+        }
+    }
+};
+</script>
+
+<style scoped>
+iframe {
+    width: 100%;
+    height: 90vh;
+}
+
+.floating-modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
+
+.modal-content {
+    width: 90vw;
+    height: 90vh;
+    background-color: white;
+    padding: 20px;
+    /* margin:30px; */
+    border-radius: 8px;
+    position: relative;
+}
+
+.close {
+    cursor: pointer;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+}
+</style>
--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,21 +1,74 @@
 <template>
-    <v-container>
+   
+    <v-card 
+        id="cardEle"
+        class="mx-auto"
+        width="80vw"
+    >
       <v-list lines="three">
         <v-list-item
-          v-for="n in 3"
-          :key="n"
-          :title="'Item ' + n"
-          subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-        ></v-list-item>
+          v-for= "(item,i) in items"
+          :key="i"
+          :title= item.title
+          :subtitle= item.subtitle
+           @click="openModal(item.embedCode)"
+        />
       </v-list>
-    </v-container>
+    </v-card>
+
+    <!-- 使用 FloatingModal 组件 -->
+    <FloatingModal 
+        :content="currentEmbedCode" 
+        :isVisible="isModalVisible" 
+        @close="closeModal" 
+    />
 </template>
 
 <script>
+    import FloatingModal from "@/components/FloatingModal.vue";
+
+    export default {
+        components:{FloatingModal},
+        data(){
+            return{
+                isModalVisible: false,        // 控制模态框显示
+                currentEmbedCode: "",         // 当前的嵌入代码
+                items: [
+                    {
+                    title: '海底世界',
+                    value: 1,
+                    subtitle:"海洋是天空的倒影，从海底来到天上，会是怎样？",
+                    embedCode: "https://1drv.ms/w/s!AlDXLATEEYkClORSDcYxyrFHeLKXQQ?embed=1&em=2"
+                    },
+                    {
+                    title: '多米诺体系中现浇楼盖的优化设计研究',
+                    value: 2,
+                    subtitle:"以3D打印模板建造的“应力线-索状” 楼盖为例",
+                    embedCode:"https://1drv.ms/b/s!AlDXLATEEYkClORVZQEXdWP6mCPjTw?embed=1&em=2"
+                    },
+                    {
+                    title: 'Item #3',
+                    value: 3,
+                    subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit"
+                    },
+                ],
+            }
+        },
+    methods: {
+        openModal(embedCode) {
+            this.currentEmbedCode = embedCode;
+            this.isModalVisible = true;
+            },
+        closeModal() {
+            this.isModalVisible = false;
+            this.currentEmbedCode = "";
+        }
+    }
+}
 </script>
 
 <style scoped>
-    .v-container{
-        width:80vw;
+    #cardEle{
+        overflow: hidden;
     }
 </style>
